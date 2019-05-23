dc5e67ee8b5b8823ada24f2755b2fb47a426d631
HanhNguyen <hanh.shin.1998@gmail.com>
2019-05-23 16:29:51 +0700

    in proccessing add word

--- src/sample/Controller/AddWordController.java
+++ src/sample/Controller/AddWordController.java
@@ -4,6 +4,10 @@ import javafx.event.ActionEvent;
 
 public class AddWordController {
 
+    @FXML
+    private TextField txt_word_custom;
+    @FXML
+    private TextArea txt_content_custom;
 
     public void pressAddDB(ActionEvent event) {
 
