a8ecd6a86e035d966134c65e6bc7cc80fc74ad73
HanhNguyen <hanh.shin.1998@gmail.com>
2019-05-23 15:40:51 +0700

    switch to add word screen

--- src/sample/Controller/MainController.java
+++ src/sample/Controller/MainController.java
@@ -5,9 +5,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.io.IOException;
 
 public class MainController implements Initializable {
 
@@ -23,5 +29,17 @@ public class MainController implements Initializable {
     @FXML
     public void closeEvent(ActionEvent actionEvent) {
         System.exit(1);
+    }
+
+    @FXML
+    public void pressAdd(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("../View/add_word.fxml"));
+        AnchorPane userview = (AnchorPane) loader.load();
+        Scene function_scene = new Scene(userview);
+
+        stage.setScene(function_scene);
+        stage.show();
     }
 }
