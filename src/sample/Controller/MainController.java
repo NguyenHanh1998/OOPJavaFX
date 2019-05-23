a80b64ffff94f16740c065bd5ee8eabf8657c2e2
Saegusa <20162230@student.hust.edu.vn>
2019-05-23 19:52:32 +0700

    update main scene close button

--- src/sample/Controller/MainController.java
+++ src/sample/Controller/MainController.java
@@ -1,60 +1,27 @@
 package sample.Controller;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.ResourceBundle;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    private Stage stage;
+public class MainController implements Initializable {
 
     @FXML
-    Button btn_Custom_Dic;
+    Button close;
 
-    public void pressEVDic(ActionEvent event) {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/functions.fxml"));
-        try {
-            Parent userview = loader.load();
-            Scene scene = new Scene(userview, 800, 600);
-            stage.setScene(scene);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());// phai load truoc moi duoc goi getController
-        }
-        //SortViewController sortController =((SortViewController) loader.getController());
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // init data in here
+        // gia su chay data ban dau la
     }
 
-    public void pressVEDic(ActionEvent event){}
-    public void pressCustomDic()throws Exception{
-        stage = (Stage) btn_Custom_Dic.getScene().getWindow();
-        BorderPane root;
-        root = (BorderPane) FXMLLoader.load(getClass().getResource("functions.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        System.out.println("button custom clicked");
+    @FXML
+    public void closeEvent(ActionEvent actionEvent) {
+        System.exit(1);
     }
-
 }
